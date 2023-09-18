9419eb7b860eafa86a2508e92f2c85164640a1c8
Minikbusk <minikbusk@hotmail.com>
2023-09-18 10:32:48 +0200

    _

--- src/ordinationstuderende/ordination/DagligFast.java
+++ src/ordinationstuderende/ordination/DagligFast.java
@@ -1,5 +1,50 @@
 package ordinationstuderende.ordination;
 
-public class DagligFast {
-    // TODO
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DagligFast extends Ordination{
+    public DagligFast(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
+
+    private final Dosis[] doser = new Dosis[4];
+
+    public Dosis[] getDoser() {
+        return doser;
+    }
+    public Dosis opretDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid, antal);
+        if (tid == LocalTime.of(6,0)) {
+            doser[0] = dosis;
+        } else if (tid == LocalTime.of(12,0)) {
+            doser[1] = dosis;
+        } else if (tid == LocalTime.of(18,0)) {
+            doser[2] = dosis;
+        } else if (tid == LocalTime.of(0,0)) {
+            doser[3] = dosis;
+        } else throw new RuntimeException("Ikke gyldigt tidspunkt. Vælg enten kl.6, 12, 18, eller 0.");
+
+        return dosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        double samletDosis = antalDage() * doegnDosis();
+        return samletDosis;
+    }
+
+    @Override
+    public double doegnDosis() {
+        int antal = 0;
+        for (Dosis d : doser) {
+            antal += (int) d.getAntal();
+        }
+        return antal;
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig Fast";
+    }
 }
--- src/ordinationstuderende/ordination/DagligSkaev.java
+++ src/ordinationstuderende/ordination/DagligSkaev.java
@@ -1,12 +1,38 @@
 package ordinationstuderende.ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination{
+    public final ArrayList<Dosis> doser = new ArrayList();
 
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        Dosis dosis = new Dosis(tid, antal);
+        doser.add(dosis);
+    }
+
+    @Override
+    public double samletDosis() {
+        double samletDosis = antalDage() * doegnDosis();
+        return samletDosis;
+    }
+
+    @Override
+    public double doegnDosis() {
+        double antal = 0;
+        for (Dosis d : doser) {
+            antal += d.getAntal();
+        }
+        return antal;
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig Skæv";
     }
 }
